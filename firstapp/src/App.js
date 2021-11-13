19685b89bf783434c6bdc5fa6703e2139d79cb52
Sirine-bg <sirinebengabsia@gmail.com>
2021-11-13 07:42:43 +0100

    first one

---
+++ firstapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import {Profile} from './components/Profile';
+function App() {
+  return (
+   <>
+   
+    <NavBar/>
+   <Profile/>
+       </>
+  );
+
+}
+
+export default App;
---
+++ firstapp/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Profile = () => {
+    return (
+        <div>
+            <p>Hi my name is wassim </p>
+        </div>
+    )
+}
